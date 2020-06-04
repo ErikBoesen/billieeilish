be0c4838cced3283c67a7497e702139803c6ca0b
ErikBoesen <me@erikboesen.com>
2020-06-04 02:24:41 -0400

    Add script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const FIRST_NAMES = ['Bully', 'Bally', 'Building', 'Bailey', 'Blunted', 'Beaver', 'Beckon', 'Bedevil', 'Becoming', 'Becometh', 'Becquerel', 'Bedazzle', 'Bedfordshire', 'Bedrabbling', 'Bedroom', 'Bedspread', 'Beezlebub', 'Befuddler', 'Baghdad', 'Begroaning'],
+      LAST_NAMES = ['Eyelash', 'Eiffel', 'Earwig', 'Easter', 'Evisceration', 'Extract', 'Excalibur', 'Excursion', 'Executioner', 'Exemlify', 'Embezzlement', 'Extortion'];
+
+function choose(array) {
+    return array[Math.floor(Math.random() * array.length)],
+}
+
+function randomName() {
+    let first_name = choose(FIRST_NAMES),
+        last_name  = choose(LAST_NAMES);
+    return first_name + ' ' + last_name;
+}
+
+document.getElementById('generate').onclick = function() {
+    document.getElementById('name').textContent = randomName();
+};
